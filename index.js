3737ea0a5a74623aacb7a17cf8c4c38b4895ce19
T0nyBlue <18521123@gm.uit.edu.vn>
2021-10-28 00:28:59 +0700

    API docs

--- index.js
+++ index.js
@@ -1,17 +1,56 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const swaggerUi = require("swagger-ui-express");
+const swaggerJsondoc = require("swagger-jsdoc");
+
 const musicsRoutes = require('./src/routes/musicsRoutes.js');
 
 const app = express();
 const port = 3636;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true}));
+const options = {
+    swaggerDefinition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Musics Management API",
+        version: "1.0.0",
+        description:
+          "Musics API document for Music management",
+        contact: {
+          name: "Bao Nam",
+          url: "https://github.com/T0nyBlue",
+          email: "18521123@gm.uit.edu.vn"
+        }
+      },
+      servers: [
+        {
+          url: "http://localhost:3636/"
+        }
+      ]
+    },
+    apis: ["./api-method-docs.js"]
+}
+const swaggerDocs = swaggerJsondoc(options);
 
-musicsRoutes(app);
+app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+app.use(bodyParser.urlencoded({ extended: true}));
+app.use(bodyParser.json());
+
+app.get("/hello", (req, res) => {
+    res.json("Hello world!");
+});
+
+// app.get(
+//     "/docs",
+//     swaggerUi.setup(specs, {
+//       explorer: true
+//     })
+// );
 
 app.get('/', (req, res) => {
     res.send('Welcome to music management API!');
 });
+
+musicsRoutes(app);
 
 app.listen(port, () => console.log(`Server running on por: http://localhost:${port}`));
